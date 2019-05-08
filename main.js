f8f87369f9b3a7da7f9c741671174ce3761571bc
Badma <badma3581@gmail.com>
2019-05-08 23:00:57 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+
+let win;
+
+function createWindow () {
+    win = new BrowserWindow({ width: 800, height: 600, frame: true, transparent: true });
+
+    win.loadFile('index.html');
+
+    win.on('closed', () => {
+        win = null;
+    })
+}
+
+app.on('ready', function () {
+    setTimeout(createWindow, 10)
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow();
+    }
+});
