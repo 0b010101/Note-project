47d847b4c78daaf28d9566781de0c63c8eaf5493
Badma <badma3581@gmail.com>
2019-05-13 00:07:13 +0300

    fixes

--- js/close-min.js
+++ js/close-min.js
@@ -2,11 +2,12 @@ const remote = require('electron').remote;
 document.getElementById("min-btn").addEventListener("click", function(e) {
     var window = remote.getCurrentWindow();
     window.minimize();
+    console.log("min");
 });
 
 document.getElementById("close-btn").addEventListener("click", function(e) {
     var window = remote.getCurrentWindow();
-
+    console.log("cls");
     window.close();
 });
 
